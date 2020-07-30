b947560a98de83c5dd9cc86ce6a7433837b6bd28
BuildTools <unconfigured@null.spigotmc.org>
2020-07-29 20:00:08 -0700

    pixel art

--- src/_02_Pixel_Art/GridPanel.java
+++ src/_02_Pixel_Art/GridPanel.java
@@ -17,7 +17,7 @@ public class GridPanel extends JPanel{
 	private int cols;
 	
 	//1. Create a 2D array of pixels. Do not initialize it yet.
-	
+	Pixel[][] pixels;
 	private Color color;
 	
 	public GridPanel(int w, int h, int r, int c) {
@@ -34,10 +34,14 @@ public class GridPanel extends JPanel{
 		setPreferredSize(new Dimension(windowWidth, windowHeight));
 		
 		//2. Initialize the pixel array using the rows and cols variables.
-		
+		pixels = new Pixel[rows][cols];
 		
 		//3. Iterate through the array and initialize each element to a new pixel.
-		
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<cols;j++) {
+				pixels[i][j] = new Pixel(j, j);
+			}
+		}
 		
 	}
 	
@@ -55,6 +59,11 @@ public class GridPanel extends JPanel{
 		//4. Iterate through the array.
 		//   For every pixel in the list, fill in a rectangle using the pixel's color.
 		//   Then, use drawRect to add a grid pattern to your display.
-		
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<cols;j++) {
+				g.fillRect(i, j, i, j);
+				g.drawRect(i, j, i, j);
+			}
+		}
 	}
 }
