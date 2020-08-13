faf9db2d0f1df8ea9ccbbe2230bdc4e76d39f67b
BuildTools <unconfigured@null.spigotmc.org>
2020-08-12 19:58:58 -0700

    conway done

--- src/_02_Pixel_Art/GridPanel.java
+++ src/_02_Pixel_Art/GridPanel.java
@@ -15,58 +15,56 @@ public class GridPanel extends JPanel{
 	private int pixelHeight;
 	private int rows;
 	private int cols;
-	ColorSelectionPanel csp = new ColorSelectionPanel();
-	
-	//1. Create a 2D array of pixels. Do not initialize it yet.
-	Pixel[][] pixels;
+
+	// 1. Create a 2D array of pixels. Do not initialize it yet.
+	Pixel[][] pixel;
 	private Color color;
-	
+
 	public GridPanel(int w, int h, int r, int c) {
 		this.windowWidth = w;
 		this.windowHeight = h;
 		this.rows = r;
 		this.cols = c;
-		
+
 		this.pixelWidth = windowWidth / cols;
 		this.pixelHeight = windowHeight / rows;
-		
+
 		color = Color.BLACK;
-		
+
 		setPreferredSize(new Dimension(windowWidth, windowHeight));
-		
-		//2. Initialize the pixel array using the rows and cols variables.
-		pixels = new Pixel[rows][cols];
-		
-		//3. Iterate through the array and initialize each element to a new pixel.
-		for(int i=0;i<rows;i++) {
-			for(int j=0;j<cols;j++) {
-				pixels[i][j] = new Pixel(j, j);
+
+		// 2. Initialize the pixel array using the rows and cols variables.
+		pixel = new Pixel[rows][cols];
+
+		// 3. Iterate through the array and initialize each element to a new pixel.
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				pixel[i][j] = new Pixel(i * pixelWidth, j * pixelHeight);
 			}
 		}
-		
+
 	}
-	
+
 	public void setColor(Color c) {
 		color = c;
 	}
-	
+
 	public void clickPixel(int mouseX, int mouseY) {
-		//5. Use the mouseX and mouseY variables to change the color
-		//   of the pixel that was clicked. *HINT* Use the pixel's dimensions.
-		pixels[mouseX/pixelWidth][mouseY/pixelHeight].color = color;
+		// 5. Use the mouseX and mouseY variables to change the color
+		// of the pixel that was clicked. *HINT* Use the pixel's dimensions.
+		pixel[mouseX/pixelWidth][mouseY/pixelHeight].color = color;
+
 	}
-	
+
 	@Override
 	public void paintComponent(Graphics g) {
-		//4. Iterate through the array.
-		//   For every pixel in the list, fill in a rectangle using the pixel's color.
-		//   Then, use drawRect to add a grid pattern to your display.
-		for(int i=0;i<rows;i++) {
-			for(int j=0;j<cols;j++) {
-				g.setColor(Color.white);
-				g.fillRect(pixelWidth*i, pixelHeight*j, pixelWidth, pixelHeight);
-				g.setColor(Color.black);
-				g.drawRect(pixelWidth*i, pixelHeight*j, pixelWidth, pixelHeight);
+		// 4. Iterate through the array.
+		// For every pixel in the list, fill in a rectangle using the pixel's color.
+		// Then, use drawRect to add a grid pattern to your display.
+		for (int i = 0; i < pixel.length; i++) {
+			for (int j = 0; j < pixel[i].length; j++) {
+				g.setColor(pixel[i][j].color);
+				g.fillRect(pixel[i][j].x, pixel[i][j].y, pixelWidth, pixelHeight);
 			}
 		}
 	}
--- src/_03_Conways_Game_of_Life/Cell.java
+++ src/_03_Conways_Game_of_Life/Cell.java
@@ -17,18 +17,30 @@ public class Cell implements Drawable{
 		this.cellSize = size;
 	}
 	
-	//11. Complete tue liveOrDie method
+	//11. Complete the liveOrDie method
 	//    It sets isAlive to true or false based on the neighbors and 
 	//the rules of the game
 	/*
-	 * 1. Any live cell with fewer than two live nieghbours dies, as if caused by underpopulation.
-	 * 2. Any live cell with two or three live neighbours lives on to the next generation.
-	 * 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
-	 * 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+	 * 1. Any live cell with fewer than two live neighbors dies, as if caused by underpopulation.
+	 * 2. Any live cell with two or three live neighbors lives on to the next generation.
+	 * 3. Any live cell with more than three live neighbors dies, as if by overpopulation.
+	 * 4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 	 * (source: Wikipedia)
 	 * */
 	public void liveOrDie(int numNeighbors) {
-		
+		if(isAlive==true) {
+			if(numNeighbors<2||numNeighbors>3) {
+				isAlive = false;
+			}
+			if(numNeighbors==2||numNeighbors==3) {
+				isAlive = true;
+			}
+		}
+		if(isAlive==false) {
+			if(numNeighbors==3) {
+				isAlive = true;
+			}
+		}
 	}
 	
 	public int getX() {
@@ -44,11 +56,13 @@ public class Cell implements Drawable{
 	//    draws empty square if cell is dead
 	@Override
 	public void draw(Graphics g) {
-	
-		
-		
-		
-		
-		
+		if(isAlive) {
+			g.setColor(Color.BLUE);
+		}
+		else {
+			g.setColor(Color.WHITE);
+		}
+		g.fillRect(x, y, cellSize, cellSize);
+		g.drawRect(x, y, cellSize, cellSize);
 	}
 }
