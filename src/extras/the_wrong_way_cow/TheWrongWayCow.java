f8118d81d72e16cf834a8d327cbbd5cb3976d551
BuildTools <unconfigured@null.spigotmc.org>
2020-09-09 19:40:00 -0700

    done

--- src/extras/the_wrong_way_cow/TheWrongWayCow.java
+++ src/extras/the_wrong_way_cow/TheWrongWayCow.java
@@ -42,29 +42,158 @@
 package extras.the_wrong_way_cow;
 
 public class TheWrongWayCow {
+	static int U;
+	static int D;
+	static int L;
+	static int R;
 
-    public static int[] findWrongWayCow(final char[][] field) {
-        // Fill in the code to return the x,y coordinate position of the
-        // head (letter 'c') of the wrong way cow!
-    	int[] cow1;
-    	int[] cow2;
-    	int[] cow3;
-    	int[] wwc;
-        for(int i=0;i<field.length;i++) {
-        	for(int j=0;j<field[i].length;j++) {
-        		if(field[i][j]=='c') {
-        			if(i!=0) {
-        				if(field[i-1][j]=='o') {
-        					if((i-1)!=0) {
-        						if(field[i-2][j]=='w') {
-        							cow1
-        						}
-        					}
-        				}
-        			}
-        		}
-        	}
-        }
-        return null;
-    }
+
+
+	static boolean isSmallest(char val) {
+		//System.err.println(U + " " + D + " " + L + " " + R);
+		int value = -1;
+		if (val == 'U')
+			value = U;
+		if (val == 'D')
+			value = D;
+		if (val == 'L')
+			value = L;
+		if (val == 'R')
+			value = R;
+
+
+//		if (value <= U && U != 0) {
+//			if (value <= D && D != 0) {
+//				if (value <= L && L != 0) {
+//					if (value <= R && R != 0) {
+//						return true;
+//					}
+//				}
+//			}
+//		}
+		if(value == 1) return true;
+		
+		return false;
+
+	}
+
+	public static int[] findWrongWayCow(final char[][] field) {
+		
+		
+		U = 0;
+		D = 0;
+		L = 0;
+		R = 0;
+
+		int[] uLoc = { -1, -1 };
+		int[] dLoc = { -1, -1 };
+		int[] lLoc = { -1, -1 };
+		int[] rLoc = { -1, -1 };
+		
+		// Fill in the code to return the x,y coordinate position of the
+		// head (letter 'c') of the wrong way cow!
+		int cposX = -1;
+		int cposY = -1;
+		System.out.println("cow: " + field[0][0] + field[0][1] + field[0][2]);
+		System.out.println(field[0].length + " " + field.length);
+
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field[0].length; j++) {
+				// System.out.println(i + " i and j " + j);
+				if (field[i][j] == 'c') {
+
+					String down = "";
+					String left = "";
+					String right = "";
+
+					try {
+						if (("" + field[i][j] + field[i - 1][j] + field[i - 2][j]).equals("cow")) {
+							U++;
+							uLoc[0] = i;
+							uLoc[1] = j;
+							//System.out.println("U(x y)" + i + " " + j);
+
+						}
+						// System.out.println("up :" + up);
+					} catch (Exception e) {
+					}
+					try {
+
+						if (("" + field[i][j] + field[i + 1][j] + field[i + 2][j]).equals("cow")) {
+							D++;
+							dLoc[0] = i;
+							dLoc[1] = j;
+							//System.out.println("D(x y)" + i + " " + j);
+						}
+						// System.out.println("down :" + down);
+					} catch (Exception e) {
+					}
+					try {
+
+						
+						if (("" + field[i][j] + field[i][j - 1] + field[i][j - 2]).equals("cow")) {
+							L++;
+							lLoc[0] = i;
+							lLoc[1] = j;
+							//System.out.println("L(x y)" + i + " " + j);
+
+						}
+						// System.out.println("left :" + left);
+					} catch (Exception e) {
+					}
+					try {
+
+						if (("" + field[i][j] + field[i][j + 1] + field[i][j + 2]).equals("cow")) {
+							R++;
+							rLoc[0] = i;
+							rLoc[1] = j;
+							//System.out.println("R(x y)" + i + " " + j);
+
+						}
+						// System.out.println("right:" + right);
+					} catch (Exception e) {
+					}
+
+
+
+
+
+
+//					System.err.println("c");
+//					for (int j2 = -1; j2 < 2; j2++) {
+//						for (int k = -1; k < 2; k++) {
+//							if(field[i + j2][j + k] == 'o') {
+//								if(field[i + j2 + Math.abs(cposX - (i + j2))][j + k + Math.abs(cposY - (j + k))] == 'w') {
+//									
+//								}
+//							}
+//						}
+//					}
+				}
+
+			}
+		}
+
+		//System.out.println(U + " " + D + " " + L + " " + R);
+
+		if (isSmallest('U')) {
+			cposX = uLoc[0];
+			cposY = uLoc[1];
+		}
+		if (isSmallest('D')) {
+			cposX = dLoc[0];
+			cposY = dLoc[1];
+			
+		}
+		if (isSmallest('L')) {
+			cposX = lLoc[0];
+			cposY = lLoc[1];
+		}
+		if (isSmallest('R')) {
+			cposX = rLoc[0];
+			cposY = rLoc[1];
+		}
+
+		return new int[] { cposY, cposX };
+	}
 }
